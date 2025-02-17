44d6bd44c6a34ce6f47505491ca4a91622082017
JiteshJangra <jitesh5921@gmail.com>
2025-02-17 17:21:35 +0530

    user register end point

--- backend/app.js
+++ backend/app.js
@@ -3,15 +3,19 @@ dotenv.config();
 const cors = require('cors');
 
 const express = require('express');
+const userRoutes = require('./routes/user.routes');
 const app = express();
 
 
 app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
+app.use('/users', userRoutes);
 app.get('/', (req, res) => {
     res.send("Hellow");
 })
 
 
 
-module.exports = { app };
+module.exports = app;
--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,12 @@
 const http = require("http");
 const app = require("./app");
+const connectToDb = require('./db/db');
+
 const PORT = process.env.PORT || 3000;
 
 const server = http.createServer(app);
+
+connectToDb();
 
 server.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`);
